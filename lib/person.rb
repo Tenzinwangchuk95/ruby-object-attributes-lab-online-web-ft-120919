76827787136213bf426f25993d30079e3945ad2a
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-17 01:35:04 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  def name= (dog_name)
+    @name = dog_name
+    
+  end
+  
+  def name 
+    @name
+  end
+  
+  def job= (dog_breed)
+    @breed = dog_breed
+  end
+  
+  def job
+    @breed
+  end
+  
+end
